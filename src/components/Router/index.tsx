e4a31dcbca5f614e56bf1f77513a286f6e2f5f7d
Alanna <alannapaiva6@gmail.com>
2023-07-26 01:05:37 -0300

    Cria a pagina de cadastro de usuario

---
+++ src/components/Router/index.tsx
@@ -0,0 +1,25 @@
+/* import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import {
+//  Dashboard,
+  Home,
+//   LoginAdmin,
+//   LoginUser,
+//   PageRoutes,
+//   SigninAdmin,
+//   SigninUser,
+} from "../../pages"; */
+
+export const Router = () => {
+  return;
+/*     <BrowserRouter>
+      <Routes>
+        <Route path={PageRoutes.login} element={<LoginUser />} />
+        <Route path={PageRoutes.signin} element={<SigninUser />} />
+        <Route path={PageRoutes.home} element={<Home />} />
+        <Route path={PageRoutes.loginAdmin} element={<LoginAdmin />} />
+        <Route path={PageRoutes.signinAdmin} element={<SigninAdmin />} />
+        <Route path={PageRoutes.dashboard} element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>  */
+};
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Container } from "./styles";
+import 'boxicons';
+
+/* @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,400&family=Ubuntu:wght@300;400;500;700&display=swap');
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link> */
+
+function Home() {
+  return (
+    <>
+      <Container>
+        <div className="circle"></div>
+        <div className="card">
+          <div className="logo">
+            <i className="bx bx-bitcoin"></i>
+          </div>
+          <h2>Create Account</h2>
+          <form className="form">
+            <input type="text" placeholder="Name"></input>
+            <input type="email" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+            <button type="submit">SIGN UP</button>
+          </form>
+        </div>
+        <footer> Existing users, sign in <a href="#"> Here</a></footer>
+      </Container>
+    </>
+  );
+}
+export default Home;
