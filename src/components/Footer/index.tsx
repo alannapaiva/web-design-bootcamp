68052b3eae62a39f3ada986a4a8270c72497d3ff
Alanna <alannapaiva6@gmail.com>
2023-07-28 22:40:12 -0300

    Criação do componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,139 @@
+import { Container } from "./styles";
+
+export const Footer = () => {
+  return (
+    <Container>
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-top">
+            <div>
+              <h6 className="footer-title">Microsoft Store</h6>
+              <ul className="footer-list">
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    About
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Careers
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Affiliates
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Sitemap
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h6 className="footer-title">Microsoft Store</h6>
+              <ul className="footer-list">
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    About
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Careers
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Affiliates
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Sitemap
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h6 className="footer-title">Microsoft Store</h6>
+              <ul className="footer-list">
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    About
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Careers
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Affiliates
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Sitemap
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h6 className="footer-title">Microsoft Store</h6>
+              <ul className="footer-list">
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    About
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Careers
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Affiliates
+                  </a>
+                </li>
+                <li className="footer-list-item">
+                  <a href="#" className="footer-list-link">
+                    Sitemap
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <hr className="footer-divider" />
+
+          <div className="footer-bottom">
+            <span className="copyright">
+              &copy; 2022 AsmrProg. All rights reserved.
+            </span>
+            <ul className="footer-list">
+              <li className="footer-list-item">
+                <a href="#" className="footer-list-link">
+                  <i className="ri-facebook-circle-line"></i>
+                </a>
+              </li>
+              <li className="footer-list-item">
+                <a href="#" className="footer-list-link">
+                  <i className="ri-instagram-line"></i>
+                </a>
+              </li>
+              <li className="footer-list-item">
+                <a href="#" className="footer-list-link">
+                  <i className="ri-twitter-line"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </Container>
+  );
+};
